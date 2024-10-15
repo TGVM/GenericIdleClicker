8ec6f73c32bbaa821b91850ac9cc445c198db33a
TGVM <tgvm96@gmail.com>
2024-10-15 13:25:58 -0300

    currency increase per second and by clicking

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager Instance { get; private set; }
+
+    [SerializeField] private int currency;
+    [SerializeField] private int currencyPerSecond;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currency = 0;
+        currencyPerSecond = 0;
+
+
+        InvokeRepeating("CurrencyIncreasePerSecond", 0, 1.0f);
+    }
+
+
+    void CurrencyIncreasePerSecond()
+    {
+        currency += currencyPerSecond;
+    }
+
+    public void IncreaseCurrency(int value)
+    {
+        currency += value;
+    }
+
+}
